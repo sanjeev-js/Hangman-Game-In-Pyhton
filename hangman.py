b76f39965fa35f730b1884bf37830386166769ae
sanjeev kumar <sanjeev18@navgurukul.org>
2019-01-02 13:45:52 +0530

    edited hangman.py

--- hangman.py
+++ hangman.py
@@ -1,31 +1,31 @@
 import string
 from words import choose_word
 from images import IMAGES
+
 # End of helper code
-# -----------------------------------
+# ------------------
 
-# hint function
-def get_hint(secret_word, letters_guessed):
-    '''
-    secret_word: ek string word jo ki user ko guess karna hai
-    letters_guessed: ek list hai, jisme wo letters hai jo ki user nai abhi tak guess kare hai
-    returns: ek character jo abhi tak guess nahi hua hai
-    '''
 
+def get_hint(secret_word,letters_guessed):
     import random
-    letters_not_guessed = []
-    
-    index = 0
-    while (index < len(secret_word)):
-        letter = secret_word[index]
-        if letter not in letters_guessed:
-            if letter not in letters_not_guessed:
-                letters_not_guessed.append(letter)
+    letter_not_guessed=[]
+    for i in secret_word:
+        if i not in letters_guessed:
+            if i not in letter_not_guessed:
+                letter_not_guessed.append(i)
+    return random.choice(letter_not_guessed)
 
-        index += 1
 
-    return random.choice(letters_not_guessed)
+def ifValid(letter):
+    if len(letter) != 1:
+        return False
 
+    if not letter.isalpha():
+        return False
+
+    # True humne tab hi return kiya hai jab
+    # user_input ki length 1 hai aur woh character hai
+    return True
 
 def is_word_guessed(secret_word, letters_guessed):
     '''
@@ -34,11 +34,11 @@ def is_word_guessed(secret_word, letters_guessed):
     returns: return True kare agar saare letters jo ki user ne guess kiye hai wo secret_word mai hai, warna no
       False otherwise
     '''
-
     if secret_word == get_guessed_word(secret_word, letters_guessed):
         return True
 
     return False
+
 # Iss function ko test karne ke liye aap get_guessed_word("kindness", [k, n, d]) call kar sakte hai
 def get_guessed_word(secret_word, letters_guessed):
     '''
@@ -72,113 +72,77 @@ def get_available_letters(letters_guessed):
     letters_left = string.ascii_lowercase
     for i in letters_guessed:
         letters_left=letters_left.replace(i,"")
+
     return letters_left
-
-
-# invalid input fuction
-def ifValid(user_input):
-    if len(user_input) != 1:
-        return False
-
-    if not user_input.isalpha():
-        return False
-
-    # True humne tab hi return kiya hai jab
-    # user_input ki length 1 hai aur woh character hai
-    return True
-
-
 
 def hangman(secret_word):
     '''
     secret_word: string, the secret word to guess.
-
     Hangman game yeh start karta hai:
-
     * Game ki shuruaat mei hi, user ko bata dete hai ki
       secret_word mei kitne letters hai
-
     * Har round mei user se ek letter guess karne ko bolte hai
-
     * Har guess ke baad user ko feedback do ki woh guess uss
       word mei hai ya nahi
-
-    * Har round ke baad, user ko uska guess kiya hua partial word
+    * Har round ke baar, user ko uska guess kiya hua partial word
       display karo, aur underscore use kar kar woh letters bhi dikhao
       jo user ne abhi tak guess nahi kiye hai
-
     '''
     print "Welcome to the game, Hangman!"
     print "I am thinking of a word that is " + str(len(secret_word)) + " letters long."
     print ""
 
-    user_difficulty_choice = raw_input("Aap abhi kitni difficulty par yeh game khelna chahte ho?\na)\tEasy\nb)\tMedium\nc)\tHard\n\nApni choice a, b, ya c ki terms mei batayein\n")
 
-    total_lives = remaining_lives = 8
-    # images_selection_list_indices mei hum woh images ke indices
-    # store kar rahe hai, jo hume display karni hai, kyuki jab
-    # total_lives 8 se kam hogi toh hum kuch hi images dikha sakte hai
-        
-    images_selection_list_indices = [0, 1, 2, 3, 4, 5, 6, 7]
+    '''total_live:-hamane max of max jo life user ko deni hai
+        choice_chose_img:- list of index for the IMAGES which is we are importing
+        frome another file
 
-    if user_difficulty_choice not in ["a", "b", "c"]:
-        print "Aapki choice invalid hai.\nGame easy mode mei start kar rahe hai"
+        Here we are giving the dificulty level to user for make the game intrusting.'''
+    total_live=8
+    remaining_live=8
+    choice_choose_img=[0,1,2,3,4,5,6,7]
 
+    level=raw_input("Aap abhi kitni difficulty par yeh game khelna chahte ho?\na)\tEasy\nb)\tMedium\nc)\tHard\n\nApni choice a, b, ya c ki terms mei batayein\n")
+    if level=="b":
+        total_live=remaining_live=6
+        choice_choose_img=[1,2,3,4,6,7]
+    elif level=="c":
+        total_live=remaining_live=4
+        choice_choose_img=[1,3,5,7]
     else:
-        if user_difficulty_choice == "b":
-            total_lives = remaining_lives = 6
-            images_selection_list_indices = [0, 2, 3, 5, 6, 7]
-
-        elif user_difficulty_choice == "c":
-            total_lives = remaining_lives = 4
-            images_selection_list_indices = [1, 3, 5, 7]
-        
+        if level != "a":
+            print "Apne wrong level choose kiya hai.\nGame basic level se start ho raha hai__"
     letters_guessed = []
 
-    letters_guessed = []
-    remaining_lives=8
-    hit=0
-    while(total_lives >0):
+
+    while(remaining_live>0):
         available_letters = get_available_letters(letters_guessed)
         print "Available letters: " + available_letters
 
         guess = raw_input("Please guess a letter: ")
         letter = guess.lower()
-
-        if (not ifValid(letter) and letter!="hint"):
+        if letter == 'hint':
+            print "Your hint for next word is "+get_hint(secret_word,letters_guessed)
+        if ifValid(letter)==False:
             print "invalid input"
             continue
-        if hit==0:
-            if guess=="hint":
-                p=get_hint(secret_word,letters_guessed)
-                print "Your hint for next character",p 
-                hit=1
-                continue
-
         if letter in secret_word:
             letters_guessed.append(letter)
-
-        if letter in secret_word:
-            letters_guessed.append(letter)
-
             print "Good guess: " + get_guessed_word(secret_word, letters_guessed)
             print ""
 
-            if is_word_guessed(secret_word, letters_guessed) == True:
+            if is_word_guessed(secret_word, letters_guessed):
                 print " * * Congratulations, you won! * * "
                 print ""
                 break
-
         else:
             print "Oops! That letter is not in my word: " + get_guessed_word(secret_word, letters_guessed)
-            print IMAGES[images_selection_list_indices][8-total_lives]
-            print "Remaining Lives : ", total_lives
+            letters_guessed.append(letter)
             print ""
-            total_lives -= 1
-            print total_lives
+            print IMAGES[choice_choose_img[total_live-remaining_live]]
+            remaining_live-=1
     else:
         print "Sorry, you ran out of guesses. The word was " + str(secret_word) + "."
-
 # Load the list of words into the variable wordlist
 # So that it can be accessed from anywhere in the program
 secret_word = choose_word()
